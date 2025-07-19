4105c2efbd6d7604fa303675b17122973220a435
Subrat Ranjan Panda <subratranjanpanda7@gmail.com>
2025-07-19 17:26:44 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, request
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html', temperature=28, humidity=65, ammonia=15, status="Healthy")
+
+if __name__ == '__main__':
+    app.run(debug=True)
