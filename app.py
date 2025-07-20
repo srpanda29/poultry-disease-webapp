e163e1e71609fe3fe064d424a7e386b699aa590c
srpanda29 <subratranjanpanda7@gmail.com>
2025-07-20 17:25:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,36 @@
-from flask import Flask, render_template, request
-from werkzeug.utils import secure_filename
 import os
-from model import predict_disease  # this is your custom function
+from flask import Flask, render_template, request, redirect, url_for
+from model import predict_disease
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = 'static/uploads'
 
-@app.route('/', methods=['GET', 'POST'])
+UPLOAD_FOLDER = 'static/uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Home route (upload form)
+@app.route('/')
 def index():
-    prediction = None
-    if request.method == 'POST':
-        if 'image' in request.files:
-            image = request.files['image']
-            filename = secure_filename(image.filename)
-            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            image.save(filepath)
-            prediction = predict_disease(filepath)
-            return render_template('index.html', prediction=prediction, image_url=filepath)
-    return render_template('index.html', prediction=prediction)
+    return render_template('index.html')
+
+# Handle image upload and prediction
+@app.route('/predict', methods=['POST'])
+def upload_image():
+    if 'file' not in request.files:
+        return "No file part", 400
+    
+    file = request.files['file']
+    if file.filename == '':
+        return "No selected file", 400
+
+    filename = secure_filename(file.filename)
+    filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    file.save(filepath)
+
+    # Predict disease using model.py
+    result = predict_disease(filepath)
+
+    return render_template('result.html', image_path=filepath, prediction=result)
 
 if __name__ == '__main__':
     app.run(debug=True)
