e35c28ede34e9707df677d0c99cb89fc704cebcd
srpanda29 <subratranjanpanda7@gmail.com>
2025-07-20 17:21:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+import os
+from model import predict_disease  # this is your custom function
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'static/uploads'
 
-@app.route('/')
-def home():
-    return render_template('index.html', temperature=28, humidity=65, ammonia=15, status="Healthy")
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    prediction = None
+    if request.method == 'POST':
+        if 'image' in request.files:
+            image = request.files['image']
+            filename = secure_filename(image.filename)
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            image.save(filepath)
+            prediction = predict_disease(filepath)
+            return render_template('index.html', prediction=prediction, image_url=filepath)
+    return render_template('index.html', prediction=prediction)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
